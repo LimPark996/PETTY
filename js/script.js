fe3fff41ece7b505a2af9836ece3cc05bcf2342d
유미 <uji835311@gmail.com>
2025-04-01 11:10:59 +0900

    [Fix] navbar 중복 문제 해결

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("/PETTY/templates/navbar.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("navbar").innerHTML = data;
-    });
-
+  
   fetch("/PETTY/templates/footer.html")
     .then((response) => response.text())
     .then((data) => {
