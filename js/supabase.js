2676a61b6e879fb97f21dd0c4ef5951d6841f358
유미 <uji835311@gmail.com>
2025-04-01 12:24:21 +0900

    [Fix] Supabase File Path Changed

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
 // supabase.js
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 export const supabase = createClient("https://cxmqnfubrioqhvnyephd.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4bXFuZnVicmlvcWh2bnllcGhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMzOTgxOTMsImV4cCI6MjA1ODk3NDE5M30.y1ps4eLxoCc076nV-qc0KqH-CDZHMhokRVrctZX2Sn4");
